cfee319db48a624919dfd3127a119533886e90dd
Willem Larsen <willemlarsen@gmail.com>
2014-06-17 07:53:28 -0700

    finish web form

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,22 @@ $(document).ready(function() {
         var inputtedLastName = $('input#new-last-name').val();
         var inputtedAddress = $('input#new-address').val();
 
-        var new Contact = Object.create(Contact);
+        var newContact = Object.create(Contact);
         newContact.firstName = inputtedFirstName;
         newContact.lastName = inputtedLastName;
         newContact.address = inputtedAddress;
-
         
-    })
-})
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+
+            $("#show-contact h2").text(newContact.fullName());
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+            $(".address").text(newContact.address);
+        });
+
+        this.reset();
+    });
+});
