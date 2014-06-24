4ee0f5c46646061e56ab03dc6896cf6f5df0b05c
Willem Larsen <willemlarsen@gmail.com>
2014-06-24 12:33:46 -0700

    expand webform to edit contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,64 @@
-var Contact = {
-    fullName: function () {
+function Contact() {
+    this.fullName = function() {
         return this.firstName + " " + this.lastName;
-    }
+    };
+}
+
+var initializeForm = function(contact) {
+    $('input#update-first-name').val(contact.firstName);
+    $('input#update-last-name').val(contact.lastName);
+    $('input#update-address').val(contact.address);
+    $('form#edit-contact').submit(function() {
+        contact.contactLabel.removeParent();
+        event.preventDefault();
+        var replacement = {
+            firstName: $('input#update-first-name').val(),
+            lastName: $('input#update-last-name').val(),
+            address: $('input#update-address').val()
+        };
+        addContact(replacement, $("ul#contacts"));
+        this.reset();
+        $('.edit-contact').hide();
+    });
 };
 
 $(document).ready(function() {
     $('form#new-contact').submit(function(event) {
         event.preventDefault();
-
-        var inputtedFirstName = $('input#new-first-name').val();
-        var inputtedLastName = $('input#new-last-name').val();
-        var inputtedAddress = $('input#new-address').val();
-
-        var newContact = Object.create(Contact);
-        newContact.firstName = inputtedFirstName;
-        newContact.lastName = inputtedLastName;
-        newContact.address = inputtedAddress;
-        
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-        $(".contact").last().click(function() {
-            $("#show-contact").show();
-
-            $("#show-contact h2").text(newContact.fullName());
-            $(".first-name").text(newContact.firstName);
-            $(".last-name").text(newContact.lastName);
-            $(".address").text(newContact.address);
-        });
-
+        var contact = {
+            firstName: $('input#new-first-name').val(),
+            lastName: $('input#new-last-name').val(),
+            address: $('input#new-address').val()
+        };
+        addContact(contact, $("ul#contacts"));
         this.reset();
     });
 });
+
+var addContact = function(contact, addToNode) {
+    contact.fullName = function() {
+        return this.firstName + " " + this.lastName;
+    };
+    addToNode.append("<li><span class='contact'>" + contact.fullName() + "</span></li>");
+    var contactLabel = $(".contact").last();
+    contact.contactLabel = contactLabel;
+    contactLabel.click(function() {
+        $("#show-contact").show();
+
+        $("#show-contact h2").text(contact.fullName());
+        $(".first-name").text(contact.firstName);
+        $(".last-name").text(contact.lastName);
+        $(".address").text(contact.address);
+
+        $("#show-contact button").click(function(event) {
+            initializeForm(contact);
+            $("#show-contact").hide();
+            $(".edit-contact").show();
+
+        });
+    });
+    contactLabel.removeParent = function () {
+          this[0].parentNode.parentNode.removeChild(this[0].parentNode);
+    };
+    return contactLabel;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,23 @@
-describe('Contact', function () {
-	describe('fullName', function () {
-		it('combines the first and last name, separated by a space', function() {
-			var testContact = Object.create(Contact);
-			testContact.firstName = "Dolly";
-			testContact.lastName = "Parton";
-			testContact.fullName().should.equal("Dolly Parton");
-		});
-	});
+describe('Contact', function() {
+  describe('fullName', function() {
+    it('combines the first and last name, separated by a space', function() {
+      var testContact = new Contact();
+      testContact.firstName = "Dolly";
+      testContact.lastName = "Parton";
+      testContact.fullName().should.equal("Dolly Parton");
+    });
+  });
+});
+describe('domTests', function() {
+  it('writes contacts', function() {
+    var c = {
+      firstName: "troy",
+      lastName: "mcgur",
+      address: "543 adday"
+    };
+    var v = addContact(c, $("body"));
+    v[0].innerText.should.equal("troy mcgur");
+  
+    v.removeParent();
+  });
 });
